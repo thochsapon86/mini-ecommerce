383ac88647e955c8543389152fb054769ddd5eed
thochsapon <candytf822@gmail.com>
2026-02-19 15:59:20 +0700

    2

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   try {
@@ -25,6 +26,33 @@ exports.register = async (req, res) => {
     await user.save();
 
     res.status(201).json({ message: "User registered successfully" });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.json({ token });
 
   } catch (error) {
     res.status(500).json({ message: "Server error" });
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const helmet = require("helmet");
+const authMiddleware = require("./middleware/authMiddleware");
 
 dotenv.config();
 
@@ -13,10 +14,19 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
+
 // Route
 const authRoutes = require("./routes/authRoutes");
 
 app.use("/api/auth", authRoutes);
+
+app.get("/api/protected", authMiddleware, (req, res) => {
+  res.json({ message: "You are authorized!" });
+});
 
 // Connect DB
 mongoose.connect(process.env.MONGO_URI)

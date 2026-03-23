e3e5a63fbdc2343f4b36cec5373feb295f9dda3c
thochsapon86 <145741685+thochsapon86@users.noreply.github.com>
2026-03-23 21:15:34 +0700

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -37,6 +37,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
+  family: 4,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
